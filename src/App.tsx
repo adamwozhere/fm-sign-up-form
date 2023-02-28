9d4fdc2b2d4a956ad0ff0a82e51eed811c45f065
Adam Wozniak <adamwozhere@gmail.com>
2023-02-28 18:28:45 +0000

    added main and article tag

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,26 @@ import './style/App.css';
 export default function App() {
   return (
     <div className="wrapper">
-      <div className="container splitter">
-        <section className="intro">
-          <h1>Learn to code by watching others</h1>
-          <p>
-            See how experienced developers solve problems in real-time. Watching
-            scripted tutorials is great, but understanding how developers think
-            is invaluable.
-          </p>
-        </section>
-        <section className="form-section">
-          <div className="card cta">
+      <main>
+        <article className="container splitter">
+          <div className="intro">
+            <h1>Learn to code by watching others</h1>
             <p>
-              <strong>Try it free for 7 days</strong> then $20/mo. thereafter
+              See how experienced developers solve problems in real-time.
+              Watching scripted tutorials is great, but understanding how
+              developers think is invaluable.
             </p>
           </div>
-          <Form />
-        </section>
-      </div>
+          <div className="form-section">
+            <div className="card cta">
+              <p>
+                <strong>Try it free for 7 days</strong> then $20/mo. thereafter
+              </p>
+            </div>
+            <Form />
+          </div>
+        </article>
+      </main>
     </div>
   );
 }
